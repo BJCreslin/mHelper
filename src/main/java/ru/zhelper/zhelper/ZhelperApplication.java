2e9c41c9908c8ba15e7d4d2149d2cd6dc2a39fa0
bjcreslin <bjcreslin@gmail.com>
2021-08-10 10:42:10 +0700

    Maven added

---
+++ src/main/java/ru/zhelper/zhelper/ZhelperApplication.java
@@ -0,0 +1,13 @@
+package ru.zhelper.zhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zhelper/zhelper/ZhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhelper.zhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
