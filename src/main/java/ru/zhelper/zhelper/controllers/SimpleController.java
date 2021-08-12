b1a33b65d480c12d6f9843cd1a1b24cb3a77c7f2
bjcreslin <bjcreslin@gmail.com>
2021-08-12 14:17:50 +0700

    changed mapping

--- src/main/java/ru/zhelper/zhelper/controllers/SimpleController.java
+++ src/main/java/ru/zhelper/zhelper/controllers/SimpleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("api")
 public class SimpleController {
     private static final String INDEX_PAGE_NAME ="index";
 
