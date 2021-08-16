649bbfb231798f3a55c0c0deb8567c8109aa673b
bjcreslin <bjcreslin@gmail.com>
2021-08-16 13:57:07 +0700

    start parcer for 615 FZ

---
+++ src/main/java/ru/zhelper/zhelper/services/ZakupkiParser615Fz.java
@@ -0,0 +1,42 @@
+package ru.zhelper.zhelper.services;
+
+import org.jsoup.Jsoup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.zhelper.zhelper.models.Procurement;
+import ru.zhelper.zhelper.services.exceptions.BadDataParsingException;
+
+public class ZakupkiParser615Fz implements ZakupkiParser {
+    Logger logger = LoggerFactory.getLogger(ZakupkiParser615Fz.class);
+    private static final String UIN_SELECTOR = "span[class=navBreadcrumb__text]";
+    private static final String NUMBER_TO_REPLACE = "№ ";
+    private static final String REPLACEMENT = "";
+
+    private static final String BAD_DATA_EXCEPTION = "Bad data in {}.";
+    private static final String UIN = "uin";
+    private static final String PARSING = "Starting parse from html. Size {}";
+    private static final String PARSED = "Procurement {} was parsed.";
+
+    @Override
+    public Procurement parse(String html) {
+        if (logger.isDebugEnabled()) {
+            logger.debug(PARSING, html.length());
+        }
+        Procurement procurement = new Procurement();
+        procurement.setUin(getUin(html));
+//todo ---------------->Insert other parse information <------------------------
+        if (logger.isDebugEnabled()) {
+            logger.debug(PARSED, procurement);
+        }
+        return procurement;
+    }
+
+    protected String getUin(String html) {
+        try {
+            return Jsoup.parse(html).body().select(UIN_SELECTOR).first().html().replace(NUMBER_TO_REPLACE, REPLACEMENT);
+        } catch (NullPointerException exception) {
+            logger.error(BAD_DATA_EXCEPTION, UIN, exception);
+            throw new BadDataParsingException(BAD_DATA_EXCEPTION, exception);
+        }
+    }
+}
---
+++ src/test/java/ru/zhelper/zhelper/services/ZakupkiParser615FzTest.java
@@ -0,0 +1,42 @@
+package ru.zhelper.zhelper.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.zhelper.zhelper.services.exceptions.BadDataParsingException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class ZakupkiParser615FzTest {
+    private static final Logger logger = LoggerFactory.getLogger(ZakupkiParser615FzTest.class);
+    private static final String fileName = "206520000012100111.html";
+    private static final String fileBadName = "206520000012100111bad.html";
+    private static final String UIN = "206520000012100111";
+    private ZakupkiParser615Fz parser;
+    private String fineHtml;
+    private String badHtml;
+
+    @BeforeEach
+    private void init() throws IOException, URISyntaxException {
+        parser = new ZakupkiParser615Fz();
+        fineHtml = new String(Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource(fileName).toURI())));
+        badHtml = new String(Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource(fileBadName).toURI())));
+    }
+
+    @Test
+    void givenHtml_whenGetUin_getUin() {
+        String result = parser.getUin(fineHtml);
+        Assertions.assertEquals(result, UIN);
+    }
+
+    @Test
+    void givenBadHtml_whenGetUin_getException() {
+        Assertions.assertThrows(BadDataParsingException.class,
+                () -> parser.getUin(badHtml));
+    }
+}
