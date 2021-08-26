3c79dc830f737ecb995984a24af771e1badda034
user <bjcreslin@gmail.com>
2021-08-26 21:52:49 +0700

    added Extension

---
+++ zHelperChromeExtension/app.js
@@ -0,0 +1,35 @@
+const FZ_44 = "https://zakupki.gov.ru/epz/order/notice/ea44/";
+const CLASS_SELECTOR_FZ44 = "tabsNav d-flex align-items-end";
+const BUTTON_NAME = "To zHelper";
+const BUTTON_CLASS = "btn btn-primary";
+const BOOTSTRAP_LINK = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css";
+const BOOTSTRAP_INTEGRITY = "sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+const url = document.documentURI;
+
+if (url.startsWith(FZ_44)) {
+    addCss(BOOTSTRAP_LINK)
+    insertButtonFZ44();
+}
+
+function insertButtonFZ44() {
+    let buttonPlace = document.getElementsByClassName(CLASS_SELECTOR_FZ44)[0];
+    let input = document.createElement("input");
+    input.type = "button";
+    input.setAttribute("class", BUTTON_CLASS);
+    input.value = BUTTON_NAME
+    buttonPlace.appendChild(input);
+}
+
+function addCss(css) {
+    const head = document.getElementsByTagName('head')[0];
+    const s = document.createElement('style');
+    s.setAttribute('type', 'text/css');
+    s.setAttribute('integrity', 'BOOTSTRAP_INTEGRITY');
+    s.setAttribute('crossOrigin', 'anonymous');
+    if (s.styleSheet) {   // IE
+        s.styleSheet.cssText = css;
+    } else {                // the world
+        s.appendChild(document.createTextNode(css));
+    }
+    head.appendChild(s);
+}
