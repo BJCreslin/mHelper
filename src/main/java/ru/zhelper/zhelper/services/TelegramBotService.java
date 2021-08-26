fbc5b4291c5f23953802801b322652f17f6057d7
ggi-dev <ggi.mail.for.dev@gmail.com>
2021-08-26 22:53:43 +0600

    add method sendMsg

--- src/main/java/ru/zhelper/zhelper/services/TelegramBotService.java
+++ src/main/java/ru/zhelper/zhelper/services/TelegramBotService.java
@@ -1,27 +1,59 @@
 package ru.zhelper.zhelper.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 @Component
 public class TelegramBotService extends TelegramLongPollingBot {
+    @Value("${telegram.bot.username}")
+    private String username;
+    @Value("${telegram.bot.token}")
+    private String token;
 
     @Override
     public String getBotUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return token;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "чем могу помочь?");
+                    break;
+                case "/setting":
+                    sendMsg(message, "что будем настраивать?");
+                    break;
+                default:
+            }
+        }
+    }
 
+    private void sendMsg(Message message, String text) {
+        SendMessage toSendMessage = new SendMessage();
+        toSendMessage.enableMarkdown(true);
+        toSendMessage.setChatId(message.getChatId().toString());
+        toSendMessage.setReplyToMessageId(message.getMessageId());
+        toSendMessage.setText(text);
+        try {
+            execute(toSendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
