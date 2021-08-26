4728083c7d54cc1a9686234b0ae44528c7716f13
ggi-dev <ggi.mail.for.web@gmail.com>
2021-08-26 15:09:17 +0600

    add telegram dependency and empty TelegramBotService

---
+++ src/main/java/ru/zhelper/zhelper/services/TelegramBotService.java
@@ -0,0 +1,31 @@
+package ru.zhelper.zhelper.services;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+@Component
+public class TelegramBotService extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
+    }
+}
