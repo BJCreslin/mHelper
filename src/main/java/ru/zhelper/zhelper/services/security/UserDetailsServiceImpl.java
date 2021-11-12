5c2198850b6db9acb8456229dba96537b41d2b8c
bjcreslin <bjcreslin@gmail.com>
2021-11-12 17:08:30 +0700

    some fixies: 1. added class UserDetailsServiceImpl 2. added repository in classUserDetailsServiceImpl

---
+++ src/main/java/ru/zhelper/zhelper/services/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package ru.zhelper.zhelper.services.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.zhelper.zhelper.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
