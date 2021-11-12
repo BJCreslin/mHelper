d869474db79eb3e1f079683f4626ce794438dd86
bjcreslin <bjcreslin@gmail.com>
2021-11-12 22:55:23 +0700

    Added new AuthTokenFilter

--- src/main/java/ru/zhelper/zhelper/cfg/jwt/AuthTokenFilter.java
+++ src/main/java/ru/zhelper/zhelper/cfg/jwt/AuthTokenFilter.java
@@ -1,4 +1,16 @@
 package ru.zhelper.zhelper.cfg.jwt;
 
-public class AuthTokenFilter {
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
