a914e961fe706755f845be576346d33b8b1365f3
bjcreslin <bjcreslin@gmail.com>
2021-11-12 16:49:32 +0700

    some fixies: 1. Added UserDetailServiceImpl

---
+++ src/main/java/ru/zhelper/zhelper/services/security/UserDetailServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.zhelper.zhelper.services.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailServiceImpl implements UserDetails {
+    private static final long serialVersionUID = -4603089111149819076L;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
