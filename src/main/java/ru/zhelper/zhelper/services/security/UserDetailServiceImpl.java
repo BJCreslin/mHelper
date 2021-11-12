15d65f4c7b6aabbea66e823f6016aa1f0c6cd353
bjcreslin <bjcreslin@gmail.com>
2021-11-12 17:05:02 +0700

    some fixies: 1. Added true in UserDetailServiceImpl

--- src/main/java/ru/zhelper/zhelper/services/security/UserDetailServiceImpl.java
+++ src/main/java/ru/zhelper/zhelper/services/security/UserDetailServiceImpl.java
@@ -73,21 +73,21 @@ public class UserDetailServiceImpl implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
