7f13e564e18452936366c3ce7d9afa4eeb8ca194
bjcreslin <bjcreslin@gmail.com>
2021-11-12 21:52:37 +0700

    Added EnableGlobalMethodSecurity

--- src/main/java/ru/zhelper/zhelper/cfg/SecurityConfig.java
+++ src/main/java/ru/zhelper/zhelper/cfg/SecurityConfig.java
@@ -1,12 +1,14 @@
 package ru.zhelper.zhelper.cfg;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
