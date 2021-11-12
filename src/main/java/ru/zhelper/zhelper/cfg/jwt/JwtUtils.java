a08dacd0f04c4ad307c90f0a0dc6d215ebb755d8
bjcreslin <bjcreslin@gmail.com>
2021-11-12 23:34:23 +0700

    Added JwtUtils methods

--- src/main/java/ru/zhelper/zhelper/cfg/jwt/JwtUtils.java
+++ src/main/java/ru/zhelper/zhelper/cfg/jwt/JwtUtils.java
@@ -1,5 +1,10 @@
 package ru.zhelper.zhelper.cfg.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,7 +14,7 @@ import java.util.Date;
 
 @Component
 public class JwtUtils {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtils.class);
     @Value("${app.jwtSecret}")
     private String jwtSecret;
     @Value("${app.jwtExpirationMs}")
@@ -21,6 +26,22 @@ public class JwtUtils {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public boolean validateJwtToken(String jwt) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt);
+            return true;
+        } catch (MalformedJwtException e) {
+            LOGGER.error(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String jwt) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt).getBody().getSubject();
     }
 
 }
