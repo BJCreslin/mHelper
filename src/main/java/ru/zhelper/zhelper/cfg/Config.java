42be7e7a47bd2bcec32b6866365874459edeb015
bjcreslin <bjcreslin@gmail.com>
2021-11-22 16:22:53 +0700

    some fixies: 1. unused was deleted

--- src/main/java/ru/zhelper/zhelper/cfg/Config.java
+++ src/main/java/ru/zhelper/zhelper/cfg/Config.java
@@ -3,10 +3,7 @@ package ru.zhelper.zhelper.cfg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jndi.JndiTemplate;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import ru.zhelper.zhelper.exceptions.DaoException;
 
 import javax.naming.NamingException;
