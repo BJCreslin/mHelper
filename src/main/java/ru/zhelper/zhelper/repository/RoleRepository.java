ba5a892adb7c96213315977052031db0acc28781
bjcreslin <bjcreslin@gmail.com>
2021-11-23 15:26:23 +0700

    some fixies: 1. pump Role tables

--- src/main/java/ru/zhelper/zhelper/repository/RoleRepository.java
+++ src/main/java/ru/zhelper/zhelper/repository/RoleRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(ERole name);
+    Optional<Role> findByName(String name);
 }
