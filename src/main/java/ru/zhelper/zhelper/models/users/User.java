a8464832d0c5b62cde72c1ed28a5f1ac71e64f28
bjcreslin <bjcreslin@gmail.com>
2021-12-05 21:25:02 +0700

    set Eager

--- src/main/java/ru/zhelper/zhelper/models/users/User.java
+++ src/main/java/ru/zhelper/zhelper/models/users/User.java
@@ -45,7 +45,7 @@ public class User extends BaseEntity {
     @NotNull
     private boolean enabled;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
