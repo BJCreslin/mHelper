9ce4faf3f851047f360277e0605021589cbb5966
bjcrelin <bjcreslin@gmail.com>
2021-12-26 21:09:24 +0700

    some changes

--- zHelper_backend/src/main/java/ru/zhelper/ZHelperBackendApplication.java
+++ zHelper_backend/src/main/java/ru/zhelper/ZHelperBackendApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZHelperApplication extends SpringBootServletInitializer {
+public class ZHelperBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZHelperApplication.class, args);
+        SpringApplication.run(ZHelperBackendApplication.class, args);
     }
 
 }
--- zHelper_tg/src/test/java/ru/zhelper/TelegramBotApplicationTests.java
+++ zHelper_tg/src/test/java/ru/zhelper/TelegramBotApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.zhelper.tg;
+package ru.zhelper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
