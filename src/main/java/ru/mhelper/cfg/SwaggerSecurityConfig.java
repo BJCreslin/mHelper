f5cf6d4613a6ef7ceb35b8d1d5e572bd64b5a071
bjcrelin <bjcreslin@gmail.com>
2022-03-19 23:18:10 +0700

    изменения

--- src/main/java/ru/mhelper/cfg/SwaggerSecurityConfig.java
+++ src/main/java/ru/mhelper/cfg/SwaggerSecurityConfig.java
@@ -2,31 +2,24 @@ package ru.mhelper.cfg;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @Order(3)
 public class SwaggerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .antMatcher("/swagger**")
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(STATELESS)
-            .and()
-            .authorizeRequests()
-            .antMatchers("/**", "/swagger-ui/**").permitAll();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**").antMatchers("/v3/api-docs/**",
-            "/swagger-ui/**", "/swagger-ui/index.html/**");
+    public void configure(WebSecurity web) {
+        web
+            .ignoring()
+            .antMatchers(
+                "/swagger-resources/**"
+                , "/swagger-ui.html"
+                , "/v2/api-docs"
+                , "/webjars/**"
+                , "/v3/api-docs/**"
+                , "/swagger-ui/**"
+                , "/swagger-ui/index.html/**");
     }
 }
