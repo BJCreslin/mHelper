03b19779f2dbc3f6ad2b83a712fc5c864c9bb5db
bjcrelin <bjcreslin@gmail.com>
2022-03-20 16:39:04 +0700

    (strategy = GenerationType.IDENTITY)

--- src/main/java/ru/mhelper/models/BaseEntity.java
+++ src/main/java/ru/mhelper/models/BaseEntity.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Data
 public abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     private Long id;
 
