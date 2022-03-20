b6fef40cc9605bbb99cbb273315c2d73c04efaa8
bjcrelin <bjcreslin@gmail.com>
2022-03-20 10:54:34 +0700

    Profile have been added to a security config.

--- src/main/java/ru/mhelper/cfg/SwaggerSecurityConfig.java
+++ src/main/java/ru/mhelper/cfg/SwaggerSecurityConfig.java
@@ -1,10 +1,12 @@
 package ru.mhelper.cfg;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Profile("!test")
 @Configuration
 @Order(3)
 public class SwaggerSecurityConfig extends WebSecurityConfigurerAdapter {
