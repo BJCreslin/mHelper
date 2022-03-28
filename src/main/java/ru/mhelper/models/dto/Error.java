9b1a4ba7961b8b43ebc2dcb0895b3b060dc8e522
bjcrelin <bjcreslin@gmail.com>
2022-03-28 07:44:03 +0700

    ApiError have been deleted

--- src/main/java/ru/mhelper/models/dto/Error.java
+++ src/main/java/ru/mhelper/models/dto/Error.java
@@ -1,10 +1,7 @@
 package ru.mhelper.models.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
