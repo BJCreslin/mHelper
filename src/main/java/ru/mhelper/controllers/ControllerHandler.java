3129d9721e97ae2060b30662c1a84da8dde9a61d
bjcreslin <bjcreslin@gmail.com>
2022-03-29 16:04:06 +0700

    start dates

--- src/main/java/ru/mhelper/controllers/ControllerHandler.java
+++ src/main/java/ru/mhelper/controllers/ControllerHandler.java
@@ -1,6 +1,6 @@
 package ru.mhelper.controllers;
 
-import org.hibernate.exception.SQLGrammarException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,15 +9,26 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.mhelper.models.dto.Error;
 
+import javax.persistence.PersistenceException;
+
 @ControllerAdvice
 public class ControllerHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({SQLGrammarException.class})
-    protected ResponseEntity<Object> handleSQLGramarEx(SQLGrammarException ex, WebRequest request) {
+    @ExceptionHandler({PersistenceException.class})
+    protected ResponseEntity<Object> handlePersistenceEx(PersistenceException ex, WebRequest request) {
         var apiError = Error.builder()
-            .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-            .cause(ex.getMessage())
-            .message("SQLGrammarException").build();
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .cause(ex.getMessage())
+                .message("PersistenceException").build();
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({DataAccessException.class})
+    protected ResponseEntity<Object> handleDataAccessEx(DataAccessException ex, WebRequest request) {
+        var apiError = Error.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .cause(ex.getMessage())
+                .message("DataAccessException").build();
         return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
