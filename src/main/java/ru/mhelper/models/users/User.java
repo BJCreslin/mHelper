4d8132ca9a742b46f3992214fa5334b8aa700959
bjcreslin <bjcreslin@gmail.com>
2022-04-07 09:57:48 +0700

    addProcurement and removeProcurement have been added. Hash code and equals.

--- src/main/java/ru/mhelper/models/users/User.java
+++ src/main/java/ru/mhelper/models/users/User.java
@@ -1,11 +1,20 @@
 package ru.mhelper.models.users;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.ToString;
 import ru.mhelper.models.BaseEntity;
 import ru.mhelper.models.procurements.Procurement;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,10 +22,11 @@ import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Data
+@ToString
 @Table(name = "users")
 @Builder
 public class User extends BaseEntity {
@@ -97,5 +107,115 @@ public class User extends BaseEntity {
                 .password(TELEGRAM_DB_PASSWORD)
                 .email(telegramId + POSTFIX_TELEGRAM_EMAIL)
                 .enabled(true).build();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getTelegramUserId() {
+        return telegramUserId;
+    }
+
+    public void setTelegramUserId(Long telegramUserId) {
+        this.telegramUserId = telegramUserId;
+    }
+
+    public TelegramStateType getTelegramStateType() {
+        return telegramStateType;
+    }
+
+    public void setTelegramStateType(TelegramStateType telegramStateType) {
+        this.telegramStateType = telegramStateType;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public List<Procurement> getProcurements() {
+        return procurements;
+    }
+
+    public void setProcurements(List<Procurement> procurements) {
+        this.procurements = procurements;
+    }
+
+    public void addProcurement(Procurement procurement) {
+        if (Objects.nonNull(procurement)) {
+            this.procurements.add(procurement);
+            procurement.getUsers().add(this);
+        }
+    }
+
+    public void removeProcurement(Procurement procurement) {
+        if (Objects.nonNull(procurement)) {
+            this.procurements.remove(procurement);
+            procurement.getUsers().remove(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        if (!super.equals(o)) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(username, user.username)) return false;
+        if (!Objects.equals(email, user.email)) return false;
+        return Objects.equals(telegramUserId, user.telegramUserId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (telegramUserId != null ? telegramUserId.hashCode() : 0);
+        return result;
     }
 }
