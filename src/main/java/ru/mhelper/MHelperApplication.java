8d2656f2dc34c02f4d69e97a87f35a140c36603a
bjcreslin <bjcreslin@gmail.com>
2023-04-16 00:50:25 +0700

    manifest had been added

--- src/main/java/ru/mhelper/MHelperApplication.java
+++ src/main/java/ru/mhelper/MHelperApplication.java
@@ -1,5 +1,6 @@
 package ru.mhelper;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
